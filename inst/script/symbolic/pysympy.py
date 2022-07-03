c50600b5eab94bcd05ca7b41d27016b72b517f49
Jørn I. Halvorsen <jorn.halvorsen@gmail.com>
2022-07-03 23:07:41 +0200

    newpysympy

--- inst/script/symbolic/pysympy.py
+++ inst/script/symbolic/pysympy.py
@@ -23,41 +23,32 @@ readmodvar = pandas.read_csv('inst/script/symbolic/csv/parameters.csv', sep=',')
 #readmodvar = pandas.read_csv('csv/parameters.csv', sep=',') 
 sympy.var(readmodvar.iloc[:,0])
 beforems = set(dir())
-#########################################################################################################################
-cx  = [Eq(x,a/(a+b))]
-cy  = [Eq(y,c/(c+d))]
-cg  = [Eq(g,a/(a+d))]
-ch  = [Eq(h,b/(b+c))]
-cm  = [Eq(m,a/(a+c))]
-cn  = [Eq(n,b/(b+d))]
-cce = [Eq(ceta,(c+d)/(a+b))]
-cal = [Eq(alpha,(a+c)/(a+b+c+d))]
-cla = [Eq(lamda,(a+d)/(a+b+c+d))]
-com = [Eq(Omega,(a+b)/(a+b+c+d))]
-cxi = [Eq(xi,(b+d)/(a+c))]
-cga = [Eq(Gamma,(b+c)/(a+d))]
+########################################################################################################################
 #########################################################################################################################
 ### Standard form
-x_s=       cx
-alpha_s =  [Eq(alpha,Omega*x+(1-Omega)*y),Eq(alpha,(x+zeta*y)/(1+zeta)),Eq(alpha,(g+Gamma*h)/(Gamma+1)),Eq(alpha,(1)/(xi+1))]
-lambda_s = [Eq(lamda,(x+zeta*(1-y))/(zeta+1)),Eq(lamda,(1)/(Gamma+1)),Eq(lamda,(m+xi*(1-n))/(xi+1))]
-zeta_s =   [Eq(zeta,(x-alpha)/(alpha-y))]
-y_s = 	   [Eq(y,solve(alpha_s[1],y)[0])]
-##########################################################################################################################
+x_s =     [Eq(x,a/(a+b))]
+y_s =     [Eq(y,c/(c+d))]
+alpha_s = [Eq(alpha,(a+c)/(a+b+c+d)),Eq(alpha,(x+zeta*y)/(1+zeta))]
+zeta_s =  [Eq(zeta,(c+d)/(a+b)),Eq(zeta,(x-alpha)/(alpha-y))]
+lambda_s= [Eq(lamda,(a+d)/(a+b+c+d)),Eq(lamda,(x+zeta*(1-y))/(zeta+1))]
+y_s.append(Eq(y,solve(alpha_s[1],y)[0]))
+###########################################################################################################################
 ###### Oppostion form
-x_o=       cx
-alpha_o =  [Eq(alpha,(x+zeta*y)/(1+zeta))]
-lambda_o = [Eq(alpha,(x+zeta*y)/(1+zeta))]
-zeta_o =   [Eq(alpha,(x+zeta*y)/(1+zeta))]
-y_o = 	    [Eq(alpha,(x+zeta*y)/(1+zeta))]
+#Eq(alpha,Omega*x+(1-Omega)*y),Eq(alpha,(g+Gamma*h)/(Gamma+1))
+#Eq(alpha,(1)/(xi+1)),Eq(lamda,(1)/(Gamma+1)),Eq(lamda,(m+xi*(1-n))/(xi+1))
+g_o =     [Eq(g,a/(a+d))]
+h_o =     [Eq(h,b/(b+c))]
+alpha_o = [Eq(alpha,(x+zeta*y)/(1+zeta))]
+Omega_o = [Eq(Omega,(a+b)/(a+b+c+d)),Eq(alpha,(x+zeta*y)/(1+zeta))]
+Gamma_o = [Eq(Gamma,(b+c)/(a+d)),Eq(alpha,(x+zeta*y)/(1+zeta))]
 ###############################################################################################################################
 ######### Hybrid form
-x_h=       cx
-alpha_h =  [Eq(alpha,(x+zeta*y)/(1+zeta))]
-lambda_h = [Eq(alpha,(x+zeta*y)/(1+zeta))]
-zeta_h =   [Eq(alpha,(x+zeta*y)/(1+zeta))]
-y_h = 	   [Eq(alpha,(x+zeta*y)/(1+zeta))]
-############################################################################################################################
+n_h=     [Eq(n,b/(b+d))]
+m_h=     [Eq(m,a/(a+c))]
+Omega_h= [Eq(alpha,(x+zeta*y)/(1+zeta))]
+lambda_h=[Eq(alpha,(x+zeta*y)/(1+zeta))]
+xi_h=    [Eq(xi,(b+d)/(a+c)),Eq(alpha,(x+zeta*y)/(1+zeta))]
+#############################################################################################################################
 ##########################################################################################################################
 ###### Storing ###
 ##### Storing ###
