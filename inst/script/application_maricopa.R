a766746951e0ae813d8b3219169e7a63ddea1ed9
lotariohw26 <lotariohw26e@protonmail.com>
2022-07-04 16:56:41 +0200

    teastingheavy

--- inst/script/application_maricopa.R
+++ inst/script/application_maricopa.R
@@ -1,4 +1,3 @@
-#################################################################################################33
 library(ManifoldDestiny)
 library(dplyr)
 library(ggplot2)
@@ -18,36 +17,35 @@ source('R/class.R')
 snr <- 1
 set.seed(snr)
 #################################################################################################
-# Dallas
-load("~/research/ManifoldDestiny/data/dallas_sel.rda")
-gcda <- Countinggraphs(dallas_sel)
+# Maricopa
+load("~/research/ManifoldDestiny/data/maricopa_sel.rda")
+gcda <- Countinggraphs(maricopa_sel)
 gcda$sortpre()
 #### Step 1: Inspect visually
 a <- gcda$plot2d(selvp=c("x","y","alpha"),selvl=c("x_pred","y_pred","alpha_pred"))
 b <- gcda$plot2d(selvp=c("zeta"),selvl='zeta_m')
 plotly::subplot(a,b,nrows=2)
-invdallas$plotly3d(partition=1)
+gcda$plotly3d(partition=1)[1]
 #### Step 2: Rotation matrix
+dfa <- gcda$rdfc
+edal <- Estimation(dfa)
+edal$rotation()
+edal$rdfc
 #### Step 3: Regression
 #### Step 4: Prediction
 #################################################################################################33
-# Clark
-### Tab1
-#ge <- parse(text='k1*alpha+k2*h+k3')
-#load("~/research/ManifoldDestiny/data/clark_sel.rda")
-#str(clark_sel)
-#est_clark  <- Estimation(clark_sel)j
-#est_clark$rotation()
-#est_clark$estimation()
-#est_clark$plotxy()
-#load("~/Research/ManifoldDestiny/data/nevada_sel.rda")
-##################################################################################################
-# Maricopa
-#load("~/research/ManifoldDestiny/data/maricopa_sel.rda")
-#str(maricopa_sel)
-#est_maricopa  <- Estimation(maricopa_sel)
-#est_maricopa$plotly3d(selvar=c('x','y','alpha'))
-#est_maricopa$plotly3d(selvar=c('g','h','alpha'))
-#sdfinp; l()
-#gcou$sortpre(6,'alpha')
-#################################################################################################33
+###
+#filename <- 'data-raw/xlsx/Dallas Texas, Completed.xlsx'
+#openxlsx::getSheetNames(filename) 
+#rot <- openxlsx::read.xlsx(filename,sheet='Complex Rotator') 
+#xray <- openxlsx::read.xlsx(filename,sheet='X-Ray Machine') 
+#rots <- dplyr::select(rot,7:9)
+#View(xray)
+#xras <- dplyr::select(xray,1:5)
+#diffu <- 	
+#length(edal$rdfc$u)
+#length(xras$u)
+#xray
+#rots
+#l()
+#
