6241b54d5ae446da5c57afb8b244d18b848f05f4
lotariohw26 <lotariohw26e@protonmail.com>
2022-07-06 01:27:52 +0200

    xartempl

--- inst/script/stylized.R
+++ inst/script/stylized.R
@@ -19,7 +19,7 @@ snr <- 1
 set.seed(snr)
 ##################################################################################################
 #### I: Voterdatabase ###
-## Inititating
+### Inititating
 agebracketmax <- c(18,100,1000)
 regf <- 0.8
 numprec <- 200
@@ -30,45 +30,48 @@ probvset <- list(c(0.70,0.30,0.00),c(0.30,0.70,0.00))
 Znr <- c(0,1)
 vrdf$realizedgp(probv=probvset,Ztech=Znr)
 votr <- vrdf$voterrollrealized
-################################################################################################33
-#### A) Fair ###
+#################################################################################################33
+##### A) Fair ###
 gcou <- Countinggraphs(votr)
-ggt1 <- gcou$plotxy(c("x","y"))
-##ggplotly(ggt1)
-####### Tab2
-ggt2a <- gcou$plot2d(selvp=c("x","y","alpha"),selvl=c("x_pred","y_pred","alpha_pred"))
-ggt2b <- gcou$plot2d(selvp=c("zeta"),selvl='zeta_m')
-plotly::subplot(ggt2a,ggt2b,nrows=2)
-####### Tab3
-ggt3a <- gcou$resplot(resvar=c('zeta_r','alpha_res'))
-ggt3b <- gcou$resplot(resvar=c('zeta_r','y_res'))
-##plotly::subplot(ggt3a,ggt3b,nrows=2)
-####### Tab4
-##gcou$resplot(resvar=c('zeta_r','y_res'))
-####### Tab5
-gcou$plotly3d(partition=1)[[1]]
-##################################################################################################33
-#### B) Rigged ###
-#### Tab1
-##grig <- Countinggraphs(votr)
-##grig$sortpre()
-####polr[1] <- 0.3096 # 0.3469
-##polr <- polynom::polynomial(grig$polyc[[1]])
-##round(polynom::integral(polr,c(0,1)),digits=4)
-##grig$riggsta()
-##### Graphical ###
-####### Tab1
-##ggt1 <- grig$plotxy(c("x","y"))
-##ggplotly(ggt1)
-######## Tab2
-##ggt2a <- grig$plot2d(selvp=c("x","y","alpha"),selvl=c("x_pred","y_pred","alpha_pred"))
-##ggt2b <- grig$plot2d(selvp=c("zeta"),selvl='zeta_m')
+gcou$lx
+
+
+#ggt1 <- gcou$plotxy(c("x","y"))
+###ggplotly(ggt1)
+######### Tab2
+##ggt2a <- gcou$plot2d(selvp=c("x","y","alpha"),selvl=c("x_pred","y_pred","alpha_pred"))
+##ggt2b <- gcou$plot2d(selvp=c("zeta"),selvl='zeta_m')
 ##plotly::subplot(ggt2a,ggt2b,nrows=2)
-######## Tab3
+######### Tab3
 ##ggt3a <- gcou$resplot(resvar=c('zeta_r','alpha_res'))
 ##ggt3b <- gcou$resplot(resvar=c('zeta_r','y_res'))
+####plotly::subplot(ggt3a,ggt3b,nrows=2)
 ######## Tab4
 ###gcou$resplot(resvar=c('zeta_r','y_res'))
 ######## Tab5
-##grig$plotly3d(partition=1)
-####################################################################################################33
+#gcou$plotly3d(partition=1)[[1]]
+###################################################################################################33
+##### B) Rigged ###
+##### Tab1
+###grig <- Countinggraphs(votr)
+###grig$sortpre()
+#####polr[1] <- 0.3096 # 0.3469
+###polr <- polynom::polynomial(grig$polyc[[1]])
+###round(polynom::integral(polr,c(0,1)),digits=4)
+###grig$riggsta()
+###### Graphical ###
+######## Tab1
+###ggt1 <- grig$plotxy(c("x","y"))
+###ggplotly(ggt1)
+######### Tab2
+###ggt2a <- grig$plot2d(selvp=c("x","y","alpha"),selvl=c("x_pred","y_pred","alpha_pred"))
+###ggt2b <- grig$plot2d(selvp=c("zeta"),selvl='zeta_m')
+###plotly::subplot(ggt2a,ggt2b,nrows=2)
+######### Tab3
+###ggt3a <- gcou$resplot(resvar=c('zeta_r','alpha_res'))
+###ggt3b <- gcou$resplot(resvar=c('zeta_r','y_res'))
+######### Tab4
+####gcou$resplot(resvar=c('zeta_r','y_res'))
+######### Tab5
+###grig$plotly3d(partition=1)
+#####################################################################################################33
