8f9529c0e8f572e9797c83ec00078477d049c8a6
lotariohw26 <lotariohw26e@protonmail.com>
2022-07-20 17:22:47 +0200

    brev

--- inst/script/Refobjecttesting.R
+++ inst/script/Refobjecttesting.R
@@ -17,46 +17,24 @@ library(gridExtra)
 ##################################################################################################
 ### Voterrollanalysis (Based on Dr. Frank)
 ##################################################################################################
+# Voterbase
+ohio_vb <- Voterdatabase()
+ohio_vb$regvbase()
+ohio_vb$scorecard()	
+ohio_vb$predictinput()
+ohio_vb$uploadvbase()
 # Graphical
-ohio_vrg <- Voterdatabase()
-ohio_vrg$regvbase()
-ohio_vrg$scorecard()	
-ohio_vrg$predictinput()
-ohio_vrg$uploadvbase()
-
-
-##ohio_vrg$lg_pred[[3]][[3]]
-ohio_vrg$plot_keyrat()
-##ohio_vrg$lg_keyr[[3]][[2]]
-ohio_vrg$plot_histio()
-#ohio_vrg$lg_hist[[1]][[2]]
-ohio_vrg$gridarrange()
-#ohio_vrr <- Voterrollreport()
-#ohio_vrr$scorecard()
-#ohio_vrr$predictinput()
-#ohio_vrr$htmlreport()
-#p <- ggplot2::ggplot(iris,ggplot2::aes(x =Sepal.Length,y=Sepal.Width))
-#p <- p+ggplot2::geom_point(ggplot2::aes(colour=Species))+ggplot2::geom_line() + labs(title="ABC")
-#pnew <- p %>% ggedit::remove_geom('labs',1)
-#gridExtra::grid.arrange(p,pnew)
-#pnew
-#p
-#
-
-
-
-
-
-
-
-,
-
-
-
-
-
-
-
+ohio_vg <- Voterdatabase()
+ohio_vg$regvbase()
+ohio_vg$scorecard()	
+ohio_vg$predictinput()
+ohio_vg$uploadvbase()
+# Report
+ohio_vr <- Voterdatabase()
+ohio_vr$regvbase()
+ohio_vr$scorecard()	
+ohio_vr$predictinput()
+ohio_vr$uploadvbase()
 ##################################################################################################
 ### Election simulation (Based on Dr. Frank)
 ##################################################################################################
