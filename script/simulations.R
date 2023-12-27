4d556a4aef8df4225db2110c0e0dfc2db46ae3b9
joernih <jorn.halvorsen@gmail.com>
2023-12-27 13:50:17 +0100

    updpak

--- script/simulations.R
+++ script/simulations.R
@@ -85,69 +85,69 @@ probw <- c(m=0.51,s=0.10)
 probva <- c(vdm=0.7,mdm=0.4,vds=0.10,mds=0.10)
 probvb <- c(vdm=0.5,mdm=0.6,vds=0.10,mds=0.10)
 ztech <- c(0,1)	
-app_bal <- ballcastsim(dfm,probw,probva,probvb,ztech)
-######### Normal form
-app_n_rep <- selreport(app_bal,md$app0)
-app_n_out <- seloutput(app_n_rep)
-app_n_sim <- SimVoterdatabase(app_bal)
-######## Rigged example 1: standard form
-app_ex1_cou <- Countinggraphs(app_bal)
-#print(app_ex1_cou$polyc[[1]][[1]])
-app_ex1_cou$sortpre()
-app_ex1_cou$mansys(sygen=list(frm=1,pre=c("alpha","x","y"),end=c("zeta","lamda"),me=c(plnr=1,rot=0)))
-#app_ex1_cou$setres(NULL,0)
-app_ex1_cou$setres(0.23,0)
-app_ex1_cou$manimp(init_par=c(k0=0.0,k1=0.5,k2=0.5),TRUE,wn=c(0,0))
-app_ex1_out <- seloutput(selreport(app_ex1_cou$rdfc,md$app0))
-app_ex1_sim <- SimVoterdatabase(app_ex1_cou$rdfc)
-######## Rigged example 2: hybrid form
-app_ex2_cou <- Countinggraphs(app_bal)
-pri_int_ex2 <- app_ex2_cou$polyc[[1]][[1]]
-#print(app_ex2_cou$polyc[[1]][[1]])
-app_ex2_cou$sortpre()
-app_ex2_cou$mansys(sygen=list(frm=2,pre=c("alpha","g","h"),end=c("Gamma","Omega"),FALSE,me=c(plnr=1,rot=0)))
-app_ex2_cou$setres(NULL,0)
-app_ex2_cou$setres(0.23,0)
-app_ex2_cou$manimp(init_par=c(k0=0.0,k1=0.5,k2=0.5),wn=c(0,0))
-app_ex2_out <- seloutput(selreport(app_ex2_cou$rdfc,md$app0))
-app_ex2_sim <- SimVoterdatabase(app_ex2_cou$rdfc)
-###### Rigged example 3: Hybrid form
-app_ex3_cou <- Countinggraphs(app_bal)
-#pri_int_ex3 <- app_ex3_cou$polyc[[1]][[1]]
-app_ex3_cou$sortpre()
-app_ex3_cou$mansys(sygen=list(frm=1,pre=c("alpha","x","y"),end=c("zeta","lamda"),me=c(plnr=1,rot=0)))
-app_ex3_cou$setres(NULL,0)
-app_ex3_cou$setres(0.23,0)
-app_ex3_cou$manimp(init_par=c(k0=0.0,k1=0.5,k2=0.5),wn=c(0,0))
-app_ex3_out <- seloutput(selreport(app_ex3_cou$rdfc,md$app0))
-app_ex3_sim <- SimVoterdatabase(app_ex3_cou$rdfc)
+#app_bal <- ballcastsim(dfm,probw,probva,probvb,ztech)
+########## Normal form
+#app_n_rep <- selreport(app_bal,md$app0)
+#app_n_out <- seloutput(app_n_rep)
+#app_n_sim <- SimVoterdatabase(app_bal)
+######### Rigged example 1: standard form
+#app_ex1_cou <- Countinggraphs(app_bal)
+##print(app_ex1_cou$polyc[[1]][[1]])
+#app_ex1_cou$sortpre()
+#app_ex1_cou$mansys(sygen=list(frm=1,pre=c("alpha","x","y"),end=c("zeta","lamda"),me=c(plnr=1,rot=0)))
+##app_ex1_cou$setres(NULL,0)
+#app_ex1_cou$setres(0.23,0)
+#app_ex1_cou$manimp(init_par=c(k0=0.0,k1=0.5,k2=0.5),TRUE,wn=c(0,0))
+#app_ex1_out <- seloutput(selreport(app_ex1_cou$rdfc,md$app0))
+#app_ex1_sim <- SimVoterdatabase(app_ex1_cou$rdfc)
+######### Rigged example 2: hybrid form
+#app_ex2_cou <- Countinggraphs(app_bal)
+#pri_int_ex2 <- app_ex2_cou$polyc[[1]][[1]]
+##print(app_ex2_cou$polyc[[1]][[1]])
+#app_ex2_cou$sortpre()
+#app_ex2_cou$mansys(sygen=list(frm=2,pre=c("alpha","g","h"),end=c("Gamma","Omega"),FALSE,me=c(plnr=1,rot=0)))
+#app_ex2_cou$setres(NULL,0)
+#app_ex2_cou$setres(0.23,0)
+#app_ex2_cou$manimp(init_par=c(k0=0.0,k1=0.5,k2=0.5),wn=c(0,0))
+#app_ex2_out <- seloutput(selreport(app_ex2_cou$rdfc,md$app0))
+#app_ex2_sim <- SimVoterdatabase(app_ex2_cou$rdfc)
+####### Rigged example 3: Hybrid form
+#app_ex3_cou <- Countinggraphs(app_bal)
+##pri_int_ex3 <- app_ex3_cou$polyc[[1]][[1]]
+#app_ex3_cou$sortpre()
+#app_ex3_cou$mansys(sygen=list(frm=1,pre=c("alpha","x","y"),end=c("zeta","lamda"),me=c(plnr=1,rot=0)))
+#app_ex3_cou$setres(NULL,0)
+#app_ex3_cou$setres(0.23,0)
+#app_ex3_cou$manimp(init_par=c(k0=0.0,k1=0.5,k2=0.5),wn=c(0,0))
+#app_ex3_out <- seloutput(selreport(app_ex3_cou$rdfc,md$app0))
+#app_ex3_sim <- SimVoterdatabase(app_ex3_cou$rdfc)
+####################################################################################################################################################################
+##### Standard
 ###################################################################################################################################################################
-#### Standard
+####### Dr. Frank
+####countys <- seq(1,88)
+####polinclude <- c(1,2,6)
+####polreport <- 3
+####tdf <- ManifoldDestiny::Voterrollreport()
+####tdf$regvbase()
+####tdf$scorecard()
+####tdf$predictinput()
+################################################################################################################################################################
+######## Concluding Tabl
+#ctone <-'Applications'
+#formved <- c('Opposition', 
+#	     'Opposition', 
+#	     'Normal', 
+#	     'Opposition')
+#casevec <- c('Miller vs. Stavros, 2020',
+#'Gilbert vs. Sisiolak vs. Lombardo 2020',
+#'Lake vs. Hobbs, 2022',
+#'Trump vs. Biden, 2020')
+#propv_vec <- c("1rd & 2th","1rd & 2th","1rd & 2th","1rd & 2th")
+#prope_vec <- c("5th & 6th","5th & 6th","3rd & 4th","5th & 6th")
+#cou_abc <- c("Case","Form of rigg","Properties","Violations")
+#concl_appps <- data.frame(case=casevec,
+#			  rig=formved,
+#			  propn=prope_vec, 
+#			  propv=propv_vec)
 ##################################################################################################################################################################
-###### Dr. Frank
-###countys <- seq(1,88)
-###polinclude <- c(1,2,6)
-###polreport <- 3
-###tdf <- ManifoldDestiny::Voterrollreport()
-###tdf$regvbase()
-###tdf$scorecard()
-###tdf$predictinput()
-###############################################################################################################################################################
-####### Concluding Tabl
-ctone <-'Applications'
-formved <- c('Opposition', 
-	     'Opposition', 
-	     'Normal', 
-	     'Opposition')
-casevec <- c('Miller vs. Stavros, 2020',
-'Gilbert vs. Sisiolak vs. Lombardo 2020',
-'Lake vs. Hobbs, 2022',
-'Trump vs. Biden, 2020')
-propv_vec <- c("1rd & 2th","1rd & 2th","1rd & 2th","1rd & 2th")
-prope_vec <- c("5th & 6th","5th & 6th","3rd & 4th","5th & 6th")
-cou_abc <- c("Case","Form of rigg","Properties","Violations")
-concl_appps <- data.frame(case=casevec,
-			  rig=formved,
-			  propn=prope_vec, 
-			  propv=propv_vec)
-#################################################################################################################################################################
