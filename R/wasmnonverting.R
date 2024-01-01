44e13a9640c8e125997157f607e6901108b90901
joernih <jorn.halvorsen@gmail.com>
2024-01-01 23:19:12 +0100

    newNS

--- R/wasmnonverting.R
+++ R/wasmnonverting.R
@@ -28,3 +28,43 @@ py_genpolycoeff <- function(expr=NULL,solvd=NULL,solvf=NULL,eur=c(0, 0, 0),dnr=0
 #####################################################################################################
 #py_genpolycoeff(expr=NULL,solvd='z',solvf='u0',eur=c(1, 4, 2),dnr=0)
 #py_polysolver() 
+##' @export bm
+bm <- function(){
+   devtools::document()
+   system(paste0('cd ',rprojroot::find_rstudio_root_file(),'; R CMD INSTALL --preclean --no-multiarch --with-keep.source .'))
+}
+#' @export k
+k <- function(){
+    df <-  clipr::write_last_clip()
+}
+
+#' @export l
+l <- function(){
+    open_command <- switch(Sys.info()[['sysname']],
+                           Windows= 'open',
+                           Linux  = 'xdg-open',
+                           Darwin = 'open')
+
+    temp_file <- paste0(tempfile(), '.xlsx')
+    df <-  clipr::write_last_clip()
+    openxlsx::write.xlsx(df, file = temp_file)
+    invisible(system(paste(open_command, temp_file),
+                     ignore.stdout = TRUE, ignore.stderr = TRUE))
+}
+
+#' @export recoudatr
+recoudatr <- function(mda=NULL){
+  gsh <- googlesheets4::read_sheet(mda$sht$url,sheet=mda$sht$pgn,range=mda$sht$rng) %>%
+    data.table::setnames(new=mda$sht$cln) %>%
+    dplyr::select(-starts_with('D')) %>%
+    dplyr::mutate(P=row_number(PN)) %>%
+    dplyr::mutate(R=row_number(RN)) %>%
+    dplyr::mutate(S=!!rlang::parse_expr(mda$sht$stuv[1])) %>%
+    dplyr::mutate(T=!!rlang::parse_expr(mda$sht$stuv[2])) %>%
+    dplyr::mutate(U=!!rlang::parse_expr(mda$sht$stuv[3])) %>%
+    dplyr::mutate(V=!!rlang::parse_expr(mda$sht$stuv[4]))
+  assign(mda$nid,gsh)
+  do.call("use_data", list(as.name(mda$nid), overwrite = TRUE))
+  return(gsh)
+}
+#####################################################################################################
