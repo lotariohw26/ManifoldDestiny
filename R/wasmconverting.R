418a194af7e7b9683eb33cff5022e93cc5ce5f47
lotariohw26 <lotariohw26@protonmail.com>
2024-07-09 21:42:52 +0200

    sorted

--- R/wasmconverting.R
+++ R/wasmconverting.R
@@ -94,6 +94,98 @@ gmp <- function(terms=c("x2","xy","y2","x3","x2y","y2x","y3")){
   }
   expre
 }
+## Simulate prob
+#' @export r2simn
+r2simn <- function(nprec=300,
+		   regs=c(3.15,0.25),
+		   minmax=c(400,4000),
+		   turn=c(0.5,0.10),
+		   Invper=c(0.5,0.10),
+		   u=c(0.6,0.10),
+		   dv=c(-0.2,0.08),
+		   form=1)
+		   {
+    BL <- list(fm1=c('S','T','U','V'),fm2=c('S','V','T','U'),fm3=c('S','U','T','V'))[[form]]
+    form <- list(std=c('alpha~x+y','alpha~x+y+zeta'),
+                 std=c('alpha~g+h','alpha~g+h+gamma'),
+                 std=c('alpha~n+m','alpha~n+m+xi'))[[form]]
+    # Box
+    dfb <- data.frame(P=seq(1,nprec)) %>%
+    dplyr::mutate(genreg=10^qnorm(runif(dplyr::n()),regs[1],regs[2])) %>%
+    dplyr::mutate(R=round(ifelse(genreg>minmax[2]|genreg<minmax[1],runif(1,minmax[1],minmax[2]),genreg))) %>%
+    dplyr::mutate(Z=round(R*qnorm(runif(dplyr::n()),turn[1],turn[2]))) %>%
+    # Form
+    dplyr::mutate(!!paste0('OpT'):=round(Z*rnorm(dplyr::n(),Invper[1],Invper[2]))) %>%
+    dplyr::mutate(!!paste0('TpF'):=Z-OpT) %>% dplyr::mutate(gen_u=rnorm(n(),u[1],u[2])) %>%
+    dplyr::mutate(gen_v=rnorm(n(),dv[1],dv[2])+gen_u) %>%
+    # Ballots
+    dplyr::mutate(!!paste0(BL[1]):=round(gen_u*OpT)) %>%
+    dplyr::mutate(!!paste0(BL[2]):=round(OpT-S)) %>%
+    dplyr::mutate(!!paste0(BL[3]):=round(gen_v*TpF)) %>%
+    dplyr::mutate(!!paste0(BL[4]):=round(TpF-U))
+
+    dfb2 <- Countingprocess(dfb)$sdfc
+
+    # Prediction
+    pe_1 <- stats::predict(lm(as.formula(form[1]),data=dfb2))
+    pe_2 <- stats::predict(lm(as.formula(form[2]),data=dfb2))
+    # Comparison
+    dfc <- dfb2 %>%
+	#1 Alpha
+	dplyr::mutate(alpha_hat_1=pe_1) %>%
+	dplyr::mutate(alpha_hat_2=pe_2) %>%
+  	dplyr::mutate(TSS_1=(alpha-mean(alpha))^2) %>%
+  	dplyr::mutate(RSS_1=(alpha-alpha_hat_1)^2) %>%
+  	dplyr::mutate(sTSS_1=sum(TSS_1)) %>%
+  	dplyr::mutate(sRSS_1=sum(RSS_1)) %>%
+  	dplyr::mutate(R2_1=1-sRSS_1/sTSS_1) %>%
+	# Ballots
+  	dplyr::mutate(TSS_2=(alpha-mean(alpha))^2) %>%
+  	dplyr::mutate(RSS_2=(alpha-alpha_hat_2)^2) %>%
+  	dplyr::mutate(sTSS_2=sum(TSS_2)) %>%
+  	dplyr::mutate(sRSS_2=sum(RSS_2)) %>%
+	# Output
+  	dplyr::mutate(R2_2=1-sRSS_2/sTSS_2)
+
+    list(r2reg=unique(dfc$R2_1),receil=unique(dfc$R2_2),dfcopy=dfc)
+}
+#' @export ballcastsim
+ballcastsim <- function(
+  dfm=(function(x){data.frame(P=seq(1,x),RV=as.integer(rnorm(x,1000,30)))})(10),
+  probw=c(0.5,0.02),
+  probva=c(0.7,0.2,0.03,0.00),
+  probvb=c(0.7,0.2,0.03,0.00),
+  ztech=c(0,0)){
+  probvrnd <<- dfm |>
+    dplyr::mutate(ZV=rnorm(dplyr::n(),probw[1],probw[2])) |>
+    dplyr::mutate(N=runif(dplyr::n(),ztech[1],ztech[2])) |>
+    dplyr::mutate(p3=(1-ztech)*(1-pmax(0, pmin(1,rnorm(dplyr::n(),probva[1],probva[3]))))) |>
+    dplyr::mutate(p6=(1-ztech)*(1-pmax(0, pmin(1,rnorm(dplyr::n(),probvb[1],probvb[3]))))) |>
+    dplyr::mutate(p2=(1-p3)*pmax(0, pmin(1,rnorm(dplyr::n(),probva[2],probva[4])))) |>
+    dplyr::mutate(p5=(1-p6)*pmax(0, pmin(1,rnorm(dplyr::n(),probvb[2],probvb[4])))) |>
+    dplyr::mutate(p1=1-p2-p3) |>
+    dplyr::mutate(p4=1-p5-p6) |>
+    dplyr::select(P,RV,ZV,N,p1,p2,p3,p4,p5,p6) |>
+    dplyr::group_split(P)
+  voticp <- lapply(seq(1,length(probvrnd)),function(x){
+    inpv <- probvrnd[[x]]
+    dfa <- data.frame(P=rep(inpv$P,inpv$RV),R=inpv$RV,C=stats::rbinom(inpv$RV,1,inpv$ZV)) |>
+    dplyr::mutate(Id=dplyr::row_number()) |>
+    dplyr::group_by(Id) |>
+    dplyr::mutate(V=ifelse(C==1,sample(1:3,1,prob=c(inpv$p1,inpv$p2,inpv$p3)),sample(4:6,1,prob=c(inpv$p4,inpv$p5,inpv$p6)))) |>
+    dplyr::mutate(S=ifelse(V==1,1,0)) |>
+    dplyr::mutate(T=ifelse(V==4,1,0)) |>
+    dplyr::mutate(U=ifelse(V==2,1,0)) |>
+    dplyr::mutate(V=ifelse(V==5,1,0))
+})
+  votpc <- do.call("rbind", voticp) |>
+    dplyr::arrange(P) |>  dplyr::group_by(P)  |>
+    dplyr::select(c('P','R','S','T','U','V')) |>
+    dplyr::mutate(S=sum(S),T=sum(T),U=sum(U),V=sum(V)) |>
+    dplyr::distinct() |>
+    dplyr::mutate(Z=sum(S+T+U+V)) |>
+    dplyr::ungroup()
+}
 #########################################################################################################################################
 ##' @export selreport
 selreport <- function(
@@ -154,99 +246,6 @@ seloutput <- function(selreport=NULL){
   list(rdfc=tab0,decs=tab1,corxy=tab2,qunt=tab3,ro3d=tab4,r2li=tab5,regr=tab6,resp=tab7,cmp=tab8,md=tab9,bb=tab10,md=tab11)
 }
 #########################################################################################################################################
-#' @export ballcastsim
-ballcastsim <- function(
-  dfm=(function(x){data.frame(P=seq(1,x),RV=as.integer(rnorm(x,1000,30)))})(10),
-  probw=c(0.5,0.02),
-  probva=c(0.7,0.2,0.03,0.00),
-  probvb=c(0.7,0.2,0.03,0.00),
-  ztech=c(0,0)){
-  probvrnd <<- dfm |>
-    dplyr::mutate(ZV=rnorm(dplyr::n(),probw[1],probw[2])) |>
-    dplyr::mutate(N=runif(dplyr::n(),ztech[1],ztech[2])) |>
-    dplyr::mutate(p3=(1-ztech)*(1-pmax(0, pmin(1,rnorm(dplyr::n(),probva[1],probva[3]))))) |>
-    dplyr::mutate(p6=(1-ztech)*(1-pmax(0, pmin(1,rnorm(dplyr::n(),probvb[1],probvb[3]))))) |>
-    dplyr::mutate(p2=(1-p3)*pmax(0, pmin(1,rnorm(dplyr::n(),probva[2],probva[4])))) |>
-    dplyr::mutate(p5=(1-p6)*pmax(0, pmin(1,rnorm(dplyr::n(),probvb[2],probvb[4])))) |>
-    dplyr::mutate(p1=1-p2-p3) |>
-    dplyr::mutate(p4=1-p5-p6) |>
-    dplyr::select(P,RV,ZV,N,p1,p2,p3,p4,p5,p6) |>
-    dplyr::group_split(P)
-  voticp <- lapply(seq(1,length(probvrnd)),function(x){
-    inpv <- probvrnd[[x]]
-    dfa <- data.frame(P=rep(inpv$P,inpv$RV),R=inpv$RV,C=stats::rbinom(inpv$RV,1,inpv$ZV)) |>
-    dplyr::mutate(Id=dplyr::row_number()) |>
-    dplyr::group_by(Id) |>
-    dplyr::mutate(V=ifelse(C==1,sample(1:3,1,prob=c(inpv$p1,inpv$p2,inpv$p3)),sample(4:6,1,prob=c(inpv$p4,inpv$p5,inpv$p6)))) |>
-    dplyr::mutate(S=ifelse(V==1,1,0)) |>
-    dplyr::mutate(T=ifelse(V==4,1,0)) |>
-    dplyr::mutate(U=ifelse(V==2,1,0)) |>
-    dplyr::mutate(V=ifelse(V==5,1,0))
-})
-  votpc <- do.call("rbind", voticp) |>
-    dplyr::arrange(P) |>  dplyr::group_by(P)  |>
-    dplyr::select(c('P','R','S','T','U','V')) |>
-    dplyr::mutate(S=sum(S),T=sum(T),U=sum(U),V=sum(V)) |>
-    dplyr::distinct() |>
-    dplyr::mutate(Z=sum(S+T+U+V)) |>
-    dplyr::ungroup()
-}
-## Simulate prob
-#' @export r2simn
-r2simn <- function(nprec=300,
-		   regs=c(3.15,0.25),
-		   minmax=c(400,4000),
-		   turn=c(0.5,0.10),
-		   Invper=c(0.5,0.10),
-		   u=c(0.6,0.10),
-		   dv=c(-0.2,0.08),
-		   form=1)
-		   {
-    BL <- list(fm1=c('S','T','U','V'),fm2=c('S','V','T','U'),fm3=c('S','U','T','V'))[[form]]
-    form <- list(std=c('alpha~x+y','alpha~x+y+zeta'),
-                 std=c('alpha~g+h','alpha~g+h+gamma'),
-                 std=c('alpha~n+m','alpha~n+m+xi'))[[form]]
-    # Box
-    dfb <- data.frame(P=seq(1,nprec)) %>%
-    dplyr::mutate(genreg=10^qnorm(runif(dplyr::n()),regs[1],regs[2])) %>%
-    dplyr::mutate(R=round(ifelse(genreg>minmax[2]|genreg<minmax[1],runif(1,minmax[1],minmax[2]),genreg))) %>%
-    dplyr::mutate(Z=round(R*qnorm(runif(dplyr::n()),turn[1],turn[2]))) %>%
-    # Form
-    dplyr::mutate(!!paste0('OpT'):=round(Z*rnorm(dplyr::n(),Invper[1],Invper[2]))) %>%
-    dplyr::mutate(!!paste0('TpF'):=Z-OpT) %>% dplyr::mutate(gen_u=rnorm(n(),u[1],u[2])) %>%
-    dplyr::mutate(gen_v=rnorm(n(),dv[1],dv[2])+gen_u) %>%
-    # Ballots
-    dplyr::mutate(!!paste0(BL[1]):=round(gen_u*OpT)) %>%
-    dplyr::mutate(!!paste0(BL[2]):=round(OpT-S)) %>%
-    dplyr::mutate(!!paste0(BL[3]):=round(gen_v*TpF)) %>%
-    dplyr::mutate(!!paste0(BL[4]):=round(TpF-U))
-
-    dfb2 <- Countingprocess(dfb)$sdfc
-
-    # Prediction
-    pe_1 <- stats::predict(lm(as.formula(form[1]),data=dfb2))
-    pe_2 <- stats::predict(lm(as.formula(form[2]),data=dfb2))
-    # Comparison
-    dfc <- dfb2 %>%
-	#1 Alpha
-	dplyr::mutate(alpha_hat_1=pe_1) %>%
-	dplyr::mutate(alpha_hat_2=pe_2) %>%
-  	dplyr::mutate(TSS_1=(alpha-mean(alpha))^2) %>%
-  	dplyr::mutate(RSS_1=(alpha-alpha_hat_1)^2) %>%
-  	dplyr::mutate(sTSS_1=sum(TSS_1)) %>%
-  	dplyr::mutate(sRSS_1=sum(RSS_1)) %>%
-  	dplyr::mutate(R2_1=1-sRSS_1/sTSS_1) %>%
-	# Ballots
-  	dplyr::mutate(TSS_2=(alpha-mean(alpha))^2) %>%
-  	dplyr::mutate(RSS_2=(alpha-alpha_hat_2)^2) %>%
-  	dplyr::mutate(sTSS_2=sum(TSS_2)) %>%
-  	dplyr::mutate(sRSS_2=sum(RSS_2)) %>%
-	# Output
-  	dplyr::mutate(R2_2=1-sRSS_2/sTSS_2)
-
-    list(r2reg=unique(dfc$R2_1),receil=unique(dfc$R2_2),dfcopy=dfc)
-}
-
 #' @export SimVoterdatabase
 SimVoterdatabase <- setRefClass("SimVoterdatabase",fields=
 				   list(
@@ -1012,3 +1011,4 @@ Estimation$methods(hat_intcomp=function(){
     prc0123=100*sum(abs(compare[[comps]] - 0) <= 3)/length(compare[[comps]]))
   comdesc <<- data.frame(stats=names(vnd),values=vnd)
 })
+
