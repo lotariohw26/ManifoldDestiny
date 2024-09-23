eb9f925919d4c83f34ffafdf402f94440fd4b2c7
lotariohw26 <lotariohw26@protonmail.com>
2024-09-23 21:02:58 +0200

    testwebpage

--- script/R/testing.R
+++ script/R/testing.R
@@ -7,11 +7,10 @@ source(paste0(rprojroot::find_rstudio_root_file(),"/R/wasmconverting.R"))
 source(paste0(rprojroot::find_rstudio_root_file(),"/R/wasmnonverting.R"))
 source(paste0(rprojroot::find_rstudio_root_file(),"/R/abc.R"))
 ls(package:ManifoldDestiny)
-#aps <- apn0r
 #aps <- apn1n
 ##aps <- apn2n
 aps <- apn3n
-#aps <- apn4n
+aps <- apn4n
 adat <- aps[[1]]
 amet <- aps[[2]]
 ##########################################################################################################
