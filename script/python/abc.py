3fc7603cc884ecc99ef5d8ddc6395155acfaf703
lotariohw26 <lotariohw26@protonmail.com>
2024-09-29 21:27:09 +0200

    starpy

---
+++ script/python/abc.py
@@ -0,0 +1,5 @@
+import polysolver as plsv
+test = plsv.genpolycoeffr(["g","h","alpha"],"z=k0+k1*x+k2*y+k3*x**2+k4*x*y+k5*y**2+k6*x**3+k7*x**2*y+k8*y**2*x+k9*y**3",'alpha',[1,2,4])
+plsv.pareq(ste='(a + b)*c', a=2, b=4, c=3)
+
+
