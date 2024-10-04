29e9af06c48ccca592604e74ab9b1ecd5d6ac1bf
lotariohw26 <lotariohw26@protonmail.com>
2024-10-04 22:44:00 +0200

    startagin

--- script/R/testing.R
+++ script/R/testing.R
@@ -7,16 +7,30 @@ source(paste0(rprojroot::find_rstudio_root_file(),"/R/wasmconverting.R"))
 source(paste0(rprojroot::find_rstudio_root_file(),"/R/wasmnonverting.R"))
 source(paste0(rprojroot::find_rstudio_root_file(),"/R/abc.R"))
 ls(package:ManifoldDestiny)
-#aps <- apn1n
-#aps <- apn2n
-#aps <- apn3r
-##aps <- apn4n
+aps <- apn1n
+aps <- apn2n
+aps <- apn3r
+aps <- apn4n
 #adat <- aps[[1]]
 #amet <- aps[[2]]
 ############################################################################################################
-#slr <- selreport(aps)
-##slo <- seloutput(slr)
+slr <- selreport(aps)
+slo <- seloutput(slr)
 ###########################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 vmat <- c(1,2,4)
 plnr <- 1
 googlesheets4::gs4_auth(email="lotariohw26@gmail.com")
