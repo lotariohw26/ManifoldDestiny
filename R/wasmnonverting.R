09f3e5abd50e952329564b10284ed07c7ee81329
lotariohw26abc <lotariohw26@protonmail.com>
2024-11-14 20:07:41 +0100

    newfunct

--- R/wasmnonverting.R
+++ R/wasmnonverting.R
@@ -99,3 +99,34 @@ gre <- function(ma="lotariohw26@gmail.com",url=url,sheet=sheet,range=range){
 	gsh <- googlesheets4::read_sheet(url,sheet=sheet,range=range) 
 }
 
+#' @export dyntabulation
+dyntabulation <- function(fld="/data-raw/arizona/2024/",
+		rac=c("Presidential Electors","US Senate"),
+		aff=c("DEM","REP"),
+		vom=c('Votes_PROVISIONAL','Votes_ELECTION DAY','Votes_EARLY VOTE'),
+		sct=c('PrecinctName','Registered','ContestName','CandidateName','CandidateAffiliation','Votes_EARLY VOTE','Votes_ELECTION DAY','Votes_PROVISIONAL')){
+  cmd <- paste0('ls ',rprojroot::find_rstudio_root_file(),fld)
+  scd <- system(cmd, intern=T)
+  lsf <- scd[grep("txt", scd)]
+  sapply(1:length(lsf),function(per){
+    #per <- n1
+    flsp <-  paste0(rprojroot::find_rstudio_root_file(),fld,lsf[per])
+    losd <- data.table::fread(flsp)
+    sapply(1:length(rac),function(con){
+        snax <- losd %>% 
+		dplyr::select(all_of(sct)) %>% 
+		dplyr::filter(ContestName%in%rac[con]) %>% 
+		dplyr::filter(CandidateAffiliation%in%aff) %>%
+                dplyr::mutate(P = as.integer(factor(PrecinctName, levels = unique(PrecinctName))),.before = 1) %>%
+                dplyr::select(-CandidateAffiliation) %>%
+		dplyr::group_by(P,ContestName) %>%
+                tidyr::pivot_wider(names_from=CandidateName,values_from=c('Votes_PROVISIONAL','Votes_ELECTION DAY','Votes_EARLY VOTE')) %>% 
+                dplyr::ungroup() %>%
+                dplyr::mutate(SNAP=per,.before = 1) %>%
+                dplyr::mutate(TIME=lsf[per],.before = 1) 
+	setNames(list(snax),rac[con]) 
+    }) -> lst_rac
+  }) -> lst_rac_snp  
+  stodfl <- setNames(lapply(1:length(rac), function(i) do.call(rbind, lst_rac_snp[1, ])), rac)
+}
+
