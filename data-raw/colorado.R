6efd9defcd9b0ef5c404603014ffd1b5c52d3b3c
lotariohw26abc <lotariohw26@protonmail.com>
2025-05-13 20:03:31 +0200

    colr

---
+++ data-raw/colorado.R
@@ -0,0 +1,10 @@
+library(data.table)
+init <- "colorado/REDACTED_CVR_Export_20201130085221_354247.csv"
+late <- "colorado/REDACTED_CVR_Export_20201130085221.csv"
+colo <- fread(init)
+coln <- fread(late)
+usethis::use_data(colo, overwrite = TRUE)
+usethis::use_data(coln, overwrite = TRUE)
+
+
+
